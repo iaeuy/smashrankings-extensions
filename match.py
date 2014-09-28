87ddbf334b6a846efdcef0a7af36633808b2687d
iaeuy <iaeuy@berkeley.edu>
2014-09-27 17:35:54 -0700

    match

---
+++ match.py
@@ -0,0 +1,7 @@
+class Match(object):
+
+	def __init__(self, player1, player2, result, tournament):
+		self.player1 = player1
+		self.player2 = player2
+		self.result = result
+		self.tournament = tournament
