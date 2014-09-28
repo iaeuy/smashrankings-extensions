a59eaa2ad9bc390b2f8b6f296502c7d31fef94de
iaeuy <iaeuy@berkeley.edu>
2014-09-27 17:29:52 -0700

    more skeleton and building database

--- player.py
+++ player.py
@@ -7,8 +7,19 @@ class Player(object):
 		self.rating = rating
 
 	def get_matches(self):
+		return
 
 	def best_win(self):
+		return
 
 	def worst_loss(self):
+		return
 
+	def __str__(self):
+		return "[%s, %s, %s]" % (self.name, self.rank, self.rating)
+
+	def __repr__(self):
+		return "[%s, %s, %s]" % (self.name, self.rank, self.rating)
+
+	def __eq__(self, other):
+		return
