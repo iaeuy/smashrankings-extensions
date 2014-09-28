2a0270d7135ace3057420d313645295f5f8c94a5
Elbert Lin <elbertlin168@berkeley.edu>
2014-09-27 20:56:57 -0700

    Working front end, mostly

--- player.py
+++ player.py
@@ -3,34 +3,34 @@ import sys
 
 class Player(object):
 
-	def __init__(self, ident, name, rank, rating):
-		self.id = ident
-		self.name = name
-		self.rank = int(float(rank))
-		self.rating = rating
+    def __init__(self, ident, name, rank, rating):
+        self.id = ident
+        self.name = name
+        self.rank = int(float(rank))
+        self.rating = rating
 
-	def get_matches(self, region):
-		from match import Match
-		matches = []
-		matches_as_dicts = requests.get('http://garsh0p.no-ip.biz:5100/' + region + 
-										'/matches?player=' + self.id).json()['matches']
-		for match in matches_as_dicts:
-			matches.append(Match(self, 
-						   match['opponent_name'],
-						   match['result'], 
-						   match['tournament_name']))
-		return matches
+    def get_matches(self, region):
+        from match import Match
+        matches = []
+        matches_as_dicts = requests.get('http://garsh0p.no-ip.biz:5100/' + region + 
+                                        '/matches?player=' + self.id).json()['matches']
+        for match in matches_as_dicts:
+            matches.append(Match(self, 
+                           match['opponent_name'],
+                           match['result'], 
+                           match['tournament_name']))
+        return matches
 
-	def best_win(self, region):
-		import extension
-		best_win = None
-		best_rank = sys.maxsize
-		for match in self.get_matches(region):
-			opponent = extension.player_by_name(match.player2, region)
-			if opponent.rank < best_rank:
-				best_rank = opponent.rank
-				best_win = match
-		return best_win
+    def best_win(self, region):
+        import extension
+        best_win = None
+        best_rank = sys.maxsize
+        for match in self.get_matches(region):
+            opponent = extension.player_by_name(match.player2, region)
+            if opponent.rank < best_rank:
+                best_rank = opponent.rank
+                best_win = match
+        return best_win
 
     def worst_loss(self):
         return
@@ -38,5 +38,5 @@ class Player(object):
     def __str__(self):
         return "[%s, %s, %s]" % (self.name, self.rank, self.rating)
 
-	def __repr__(self):
-		return "[%s, %s, %s]" % (self.name, self.rank, self.rating)
+    def __repr__(self):
+        return "[%s, %s, %s]" % (self.name, self.rank, self.rating)
--- server.py
+++ server.py
@@ -1,10 +1,6 @@
-import sqlite3
-import requests
 from flask import *
 from extension import *
-from contextlib import closing
 
-DATABASE = 'smashrankings.db'
 DEBUG = True
 SECRET_KEY = 'smash4lyfe'
 URL = 'http://garsh0p.no-ip.biz:5100/'
@@ -12,62 +8,26 @@ URL = 'http://garsh0p.no-ip.biz:5100/'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = connect_db()
-    return db
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        for region in get_all_regions():
-            for player in get_players_in_region(region):
-                db.execute('insert into playerdata (name, data) values (?, ?)', [player.name, player.id])
-        db.commit()
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exeception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-
 @app.route('/')
 def show_comparison():
-    players = [dict(name=session.get('player1')['name'], id=session.get('player1')['data']), \
-            dict(name=session.get('player2')['name'], id=session.get('player2')['data'])]
-    return render_template('show_comparison.html', players=players)
+    players = [None]
+    if session.get('player1'):
+        p1 = player_by_name(session.get('player1'), session.get('region'))
+        p2 = player_by_name(session.get('player2'), session.get('region'))
+        session.pop('player1', None)
+        session.pop('player2', None)
+        if not isinstance(p1, str) or not isinstance(p2, str):
+            players = [dict(name=p1.name, rank=p1.rank), dict(name=p2.name, rank=p2.rank)]
+    return render_template('show_comparison.html', regions=get_all_regions(), players=players)
 
 @app.route('/search', methods=['POST'])
 def search_players():
-    session.pop('player1', None)
-    session.pop('player2', None)
-    p1 = request.form['player1'].lower()
-    p2 = request.form['player2'].lower()
-    cur = g.db.execute('select name from playerdata')
-    players = [row[0].lower() for row in cur.fetchall()]
-    if p1 not in players or p2 not in players:
+    session['player1'] = request.form['player1']
+    session['player2'] = request.form['player2']
+    session['region'] = request.form['region']
+    if not session.get('player1'):
         flash('Could not find player(s)')
-    else:
-        session['player1'] = query_db('select * from playerdata where name = ?', [p1], one=True)
-        session['player2'] = query_db('select * from playerdata where name = ?', [p2], one=True)
     return redirect(url_for('show_comparison'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        init_db()
     app.run()
